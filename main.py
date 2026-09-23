f5d7f9a2d493a7d693f3f69cfecacb4ebb64fece
shayaanshaikh10 <shayaanshaikhlm10@gmail.com>
2026-09-23 17:56:40 +0530

    Implement FastAPI server with a home endpoint returning server status

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from razorpay-bitrix!")
+from fastapi import FastAPI
 
+app=FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def home():
+    return {"status":"server is running"}
